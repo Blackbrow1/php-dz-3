2723c4ac3f8384137b44b5ea13458c58c5a25dbe
Alex Blackbrow <blackbrow@mail.ru>
2026-04-27 21:34:40 +0300

    fix: Исправил

--- index.php
+++ index.php
@@ -1,22 +1,24 @@
 <?php
-function fullName(string $surname, string $name, string $patronymic) 
+function formatFio(string $surname, string $name, string $patronymic) 
 {
   $surname = mb_convert_case($surname, MB_CASE_TITLE, "UTF-8");
   $name = mb_convert_case($name, MB_CASE_TITLE, "UTF-8");
   $patronymic = mb_convert_case($patronymic, MB_CASE_TITLE, "UTF-8");
   
-  $fullName = "$name $patronymic $surname";
+  $fullName = "$surname $name $patronymic";
   
-  $surnameAndInitials = "$surname " . mb_substr($name, 0, 1, "UTF-8") . "." . mb_substr($patronymic, 0, 1, "UTF-8") . ".";
+  $surnameAndInitials = $surname . " " . 
+                        mb_substr($name, 0, 1, "UTF-8") . "." . 
+                        mb_substr($patronymic, 0, 1, "UTF-8") . ".";
   
   $fio = mb_substr($surname, 0, 1, "UTF-8") . 
-                        mb_substr($name, 0, 1, "UTF-8") . 
-                        mb_substr($patronymic, 0, 1, "UTF-8");
+          mb_substr($name, 0, 1, "UTF-8") . 
+          mb_substr($patronymic, 0, 1, "UTF-8");
   
-  echo "Полное имя: $fullName" . PHP_EOL;
-  echo "Фамилия и инициалы: $surnameAndInitials" . PHP_EOL;
-  echo "Аббревиатура: $fio";
+  echo "Полное имя: '$fullName'" . PHP_EOL;
+  echo "Фамилия и инициалы: '$surnameAndInitials'" . PHP_EOL;
+  echo "Аббревиатура: '$fio'";
 }
 
-fullName('макаров', 'иннокентий', 'петрович');
+formatFio('иванов', 'иван', 'иванович');
 ?>
