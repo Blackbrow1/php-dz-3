33c927777c782cc84f4ec30539395787b6a65130
Alex Blackbrow <blackbrow@mail.ru>
2026-04-27 21:06:55 +0300

    fix: Исправил

--- index.php
+++ index.php
@@ -7,15 +7,15 @@ function fullName(string $surname, string $name, string $patronymic)
   
   $fullName = "$name $patronymic $surname";
   
-  $fio = "$surname " . mb_substr($name, 0, 1, "UTF-8") . "." . mb_substr($patronymic, 0, 1, "UTF-8") . ".";
+  $surnameAndInitials = "$surname " . mb_substr($name, 0, 1, "UTF-8") . "." . mb_substr($patronymic, 0, 1, "UTF-8") . ".";
   
-  $surnameAndInitials = mb_substr($surname, 0, 1, "UTF-8") . 
+  $fio = mb_substr($surname, 0, 1, "UTF-8") . 
                         mb_substr($name, 0, 1, "UTF-8") . 
                         mb_substr($patronymic, 0, 1, "UTF-8");
   
   echo "Полное имя: $fullName" . PHP_EOL;
-  echo "Фамилия и инициалы: $fio" . PHP_EOL;
-  echo "Аббревиатура: $surnameAndInitials";
+  echo "Фамилия и инициалы: $surnameAndInitials" . PHP_EOL;
+  echo "Аббревиатура: $fio";
 }
 
 fullName('макаров', 'иннокентий', 'петрович');
